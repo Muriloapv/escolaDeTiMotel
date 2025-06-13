24ddb17934dae72a15803b12c893802021ffe7e9
Muriloapv <muriloalves121@outlook.com>
2025-06-13 14:24:06 -0300

    CORS: instalado o pacote responsavel por enviar os dados

--- apiMovimentacao/app.js
+++ apiMovimentacao/app.js
@@ -6,7 +6,9 @@ const app         = express();
 const movimentacaoRouter = require( './router/movimentacaoRouter' );
 const quartoRouter       = require( './router/quartosRouter'      );
 const PORT               = process.env.PORT || 8080;
+const cors = require('cors');
 
+app.use(cors());
 app.use( express.json() );
 app.use( '/api-doc', swaggerUi.serve, swaggerUi.setup( swaggerFile ) );
 app.use( '/motel'  , movimentacaoRouter );
